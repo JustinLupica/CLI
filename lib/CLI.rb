b95c3662440b325ae5ee302401bd30c7318cbf22
JustinLupica <justinlupica@gmail.com>
2020-09-06 14:04:24 -0400

    Updated bin and gemspec

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -3,3 +3,7 @@ require_relative "./CLI/api.rb"
 require_relative "./CLI/controller.rb"
 require_relative "./CLI/recipes.rb"
 require_relative "./CLI/tags.rb"
+
+require 'pry'
+require 'open-uri'
+require 'json'
