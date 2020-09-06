b36d9a976b14ff11eda92026ba8937198c6f300e
JustinLupica <justinlupica@gmail.com>
2020-09-06 14:53:37 -0400

    update controller

--- lib/CLI/controller.rb
+++ lib/CLI/controller.rb
@@ -4,12 +4,12 @@ class Controller
     end
     def call
         input = nil
-        # while input != 'exit'
             puts "Welcome!"
             puts "Here are all the categories you can browse recipes for:"
                 API.tags_list_api   
                 Tags.display_all_tags 
             #Tags.display_all_tags
+            while input != 'exit' or input.to_i > 0
             puts "Enter the number of the category you would like:"
             input = gets.strip.downcase
             if input.to_i > 0
@@ -25,15 +25,15 @@ class Controller
                 puts "Not sure which one you want.. "
                 puts "Type 'categories' or 'exit':"
             end
-        #end
+        end
     end
 
     def recipes
         input = nil
-        while input != 'exit'
             Recipes.display_all_recipes
             puts "Here are some recipes you may like!"
             puts "Enter the number of the recipe you would like to view:"
+            while input != 'exit'
             input = gets.strip.downcase
             if input.to_i > 0
                 i = input.to_i
@@ -44,9 +44,13 @@ class Controller
                     puts Recipes.all_recipes[i-1].description
                 end
                 puts "Step by Step video tutorial: #{Recipes.all_recipes[i-1].video_url}"
-                input = gets.strip.downcase
+                puts "Type 'list to see the recipes again,"
+                puts "or type 'exit' to quit"
+                input2 = gets.strip.downcase
             elsif input == 'list'
                 Recipes.display_all_recipes
+                puts "Type 'list' if you would like to see the recipes again,"
+                puts "or type 'exit' to quit"
             elsif input == "exit"
                 goodbye
             else
